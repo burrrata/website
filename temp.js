e9c1f2ca960207f467c176b2091c4b45fda33708
anon <42473928+burrrata@users.noreply.github.com>
2019-06-13 18:08:28 +0000

    _

--- temp.js
+++ temp.js
@@ -1,32 +1,53 @@
 // a generic component that takes in parameters to display
-const HighlevelHighlight = (props) => (
-    <div style={{
-      //border: '1vmin solid blue',
-      minWidth: '33vmin',
-      maxWidth: '50vmin',
-      padding: '3vmin',
-      flex: 'auto',
-      }}>
-      <img style={{
-        padding: '1vmin',
-        width: '21vmin',
-        height: '21vmin',
-        borderRadius: '50%',
-        }}
-        src={props.image} />
-      <h2 style={{
-        padding: '1vmin',
-      }}>
-        {props.title}
-      </h2>
-      <p style={{
-        padding: '1vmin',
-      }}>
-        {props.description}
-      </p>
-    </div>
-);
-
+const Highlight = (props) => (
+		<div style={{
+        //background: 'black',  
+			  //border: '2px solid red',
+				margin: '0 auto',
+				display: 'flex',
+				flexFlow: 'row nowrap',
+				flex: 'auto',
+				flexDirection: 'row',
+				justifyContent: 'space-between',
+				padding: '2vmin',
+				textAlign: 'center',
+				alignItems: 'center',
+				fontSize: '3vmin',
+				wordWrap: 'break-word',
+        minHeight: '50vmin',
+        minWidth: '80vmin',
+				}}>
+				<div style={{
+            //background: 'blue',
+            //color: '#fff',
+            flex: '0 0 50%',
+						}}>
+          <div style={{
+						//paddingTop: '5vmin',
+            }}>
+						<a href={props.link}>
+							<h2>
+								{props.title}
+							</h2>
+						</a>
+            <p>
+              {props.description}
+            </p>
+          </div>
+				</div>
+				<div style={{
+            //background: 'green',
+            //color: '#fff',
+						flex: '0 0 50%',
+						}}>
+				<a href={props.link}>
+					<img style={{
+								width: '33vmin',
+								height: '33vmin',
+								borderRadius: '50%',
+							}}
+							src={props.image} />
+					</a>
 
 
 // gets the URL for the latest APOD image and displays it in an img
