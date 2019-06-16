d1c3792c926b56e58c8076287150dc57c8afa221
burrrata <42473928+burrrata@users.noreply.github.com>
2019-06-16 02:21:59 +0000

    updated footer and links

--- v2-test/docusaurus.config.js
+++ v2-test/docusaurus.config.js
@@ -65,12 +65,20 @@ module.exports = {
               href: 'https://www.reddit.com/user/burrrata',
             },
 						{
+							label: 'Keybase',
+							href: 'https://keybase.io/burrrata'
+						},
+						{
 							label: 'Riot',
 							href: '@burrrata:matrix.org'
 						},
 						{
-							label: 'Keybase',
-							href: 'https://keybase.io/burrrata'
+							label: 'ethresear.ch',
+							href: 'https://ethresear.ch/u/burrrata/summary',
+						},
+						{
+							label: 'forum.aragon.org',
+							href: 'https://forum.aragon.org/u/burrrata/summary',
 						},
           ],
         },
@@ -79,16 +87,16 @@ module.exports = {
           items: [
 						{
 							label: 'Projects',
-							to: 'docs/doc1',
+							to: 'projects',
+						},
+						{
+							label: 'GitHub',
+							href: 'https://github.com/burrrata',
 						},
             {
               label: 'Blog',
               to: 'blog',
             },
-						{
-							label: 'GitHub',
-							href: 'https://github.com/burrrata',
-						},
           ],
         },
       ],
--- v2-test/pages/highlevel-highlights.js
+++ v2-test/pages/highlevel-highlights.js
@@ -1,35 +1,27 @@
 import React from 'react';
 
 
-// test data
-const testData = {
-	title: 'Test Data',
-	description: 'Filler text is text that is random or otherwise generated.',
-	image: 'https://apod.nasa.gov/apod/image/1905/Trumpler14_Hubble_960.jpg',
-};
-
 // highlevel highlights data
 const highlevelHighlightsData = [
 	// highlevel highlight 1
 	{
-	title: 'Highlight 1',
-	description: 'Filler text is text that is random or otherwise generated.',
+	title: 'Align Incentives',
+	description: 'Enhance your community governance and engagement via cryptoeconomic token models',
 	image: 'https://apod.nasa.gov/apod/image/1905/Trumpler14_Hubble_960.jpg',
 	},
 	// highlevel highlight 2
 	{
-		title: 'Highlight 2',
-		description: 'Filler text is text that shares some characteristics of a real written text.',
+		title: 'Bring Apps To Life',
+		description: 'Leverage the power of the browser to deliver fast and secure machine learning experiences',
 		image: 'https://apod.nasa.gov/apod/image/1903/F_JellyFish_FIN_APOD1024.jpg',
 	},
 	// highlevel highlight 3
 	{
-		title: 'Highlight 3',
-		description: 'Filler text is text that shares some characteristics of a real written text, but is random or otherwise generated.',
+		title: 'Keep It Simple',
+		description: 'Delight users and increase engagement with simple, beautiful, and welcoming apps',
 		image: 'https://apod.nasa.gov/apod/image/1905/CarinaWideField_Willasch_1080.jpg',
 	},
 ];
-
 
 // a generic component that takes in parameters to display
 const HighlevelHighlight = (props) => (
